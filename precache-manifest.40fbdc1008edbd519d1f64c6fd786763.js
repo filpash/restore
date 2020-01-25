325cbfdc22ed76d4cf459f01d53a053ca8af28b6
Filipp <fili92@mail.ru>
2020-01-25 17:53:57 +0300

    Updates

---
+++ precache-manifest.40fbdc1008edbd519d1f64c6fd786763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab75b5b46d9a4736f8af035f1afc481a",
+    "url": "/restore/index.html"
+  },
+  {
+    "revision": "7ba98e54dfefacc8c677",
+    "url": "/restore/static/css/main.ab413278.chunk.css"
+  },
+  {
+    "revision": "130e71614f766b12a669",
+    "url": "/restore/static/js/2.b54944ca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restore/static/js/2.b54944ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ba98e54dfefacc8c677",
+    "url": "/restore/static/js/main.0fce7418.chunk.js"
+  },
+  {
+    "revision": "242f30eaef6fe52b6332",
+    "url": "/restore/static/js/runtime-main.f975a117.js"
+  }
+]);
